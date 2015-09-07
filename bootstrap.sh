9e3ea2ae11137b54e909090e669ac842079cf5d4
Bartul Bonacic <bartul.bonacic@live.com>
2015-09-07 15:01:43 +0400

    Initial setup

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# apt-get update
+# apt-get upgrade
+
+# install nginx
+sudo -s
+nginx=stable # use nginx=development for latest development version
+ubuntu=trusty # Replace this with your ubuntu version!
+echo "deb http://ppa.launchpad.net/nginx/$nginx/ubuntu $ubuntu main" > /etc/apt/sources.list.d/nginx-$nginx-$ubuntu.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C300EE8C
+apt-get update 
+apt-get install -y nginx

994e3d5e08ed01046d93021a23ab671714ce9960
Bartul Bonacic <bartul.bonacic@live.com>
2016-06-03 13:16:09 +0200

    install script output better and with color

--- nginx/bootstrap.sh
+++ nginx/bootstrap.sh
@@ -3,7 +3,7 @@
 # apt-get update
 # apt-get upgrade
 
-echo #Installing nginx 
+echo  -e '\e[0;32mInstalling nginx'
 sudo -s
 nginx=stable # use nginx=development for latest development version
 ubuntu=trusty # Replace this with your ubuntu version!
@@ -12,12 +12,12 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C300EE8C
 apt-get update 
 apt-get install -y nginx
 
-echo #Install essentials
+echo  -e '\e[0;32mInstalling essentials'
 apt-get install -y curl
 apt-get install -y vim
 
-echo #Copying nginx config for web site
+echo  -e '\e[0;32mCopying nginx config for static web site'
 sudo cp /vagrant/nginx-config/default /etc/nginx/sites-enabled/default
 
-echo #Relaod nginx config
+echo  -e '\e[0;32mReload nginx config'
 sudo nginx -s reload 

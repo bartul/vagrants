af93ee179197354c05af008ae1e6159d6f0ad3fd
Bartul Bonacic <bartul.bonacic@live.com>
2016-06-20 10:14:46 +0200

    Replace varnish with nginx in eventstore setup

--- eventstore/install-eventstore.sh
+++ eventstore/install-eventstore.sh
@@ -19,14 +19,35 @@ apt-get install varnish -y
 echo -e '\e[0;32mStarting Eventstore'
 sudo service eventstore start
 
-echo -e '\e[0;32mCopying Varnish config'
-sudo cp /vagrant/varnish-config/varnish /etc/default/
-sudo cp /vagrant/varnish-config/default.vcl /etc/varnish/
+
+echo  -e '\e[0;32mInstalling nginx'
+sudo -s
+nginx=stable # use nginx=development for latest development version
+ubuntu=trusty # Replace this with your ubuntu version!
+echo "deb http://ppa.launchpad.net/nginx/$nginx/ubuntu $ubuntu main" > /etc/apt/sources.list.d/nginx-$nginx-$ubuntu.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C300EE8C
+apt-get update
+apt-get install -y nginx
+
+
+echo  -e '\e[0;32mCopying nginx config'
+sudo cp /vagrant/nginx-config/default /etc/nginx/sites-enabled/default
+
+echo  -e '\e[0;32mReload nginx config'
+sudo nginx -s reload
+
+####################################################
+# VARNISH DID NOT WORK, SO USING TO NGINX
+####################################################
+
+# echo -e '\e[0;32mCopying Varnish config'
+# sudo cp /vagrant/varnish-config/varnish /etc/default/
+# sudo cp /vagrant/varnish-config/default.vcl /etc/varnish/
 
 # echo -e '\e[0;32mRestarting Varnish'
 # sudo service varnish restart
 
-echo -e '\e[0;32mStarting Varnish (HACK)'
-sudo /usr/sbin/varnishd -f /etc/varnish/default.vcl -a :1337 -T 127.0.0.1:6082 -S /etc/varnish/secret -s malloc,256MB
+# echo -e '\e[0;32mStarting Varnish (HACK)'
+# sudo /usr/sbin/varnishd -f /etc/varnish/default.vcl -a :1337 -T 127.0.0.1:6082 -S /etc/varnish/secret -s malloc,256MB
 # echo -e '\e[0;32mRebooting system'  
 # sudo reboot -n

4ca2621f670f14307c10a3f76fc8b6409956f73e
Bartul Bonacic <bartul.bonacic@live.com>
2016-06-27 16:26:20 +0200

    Add Redis setup without nginx

--- redis/install-redis.sh
+++ redis/install-redis.sh
@@ -14,19 +14,9 @@ wget --quiet -O - http://www.dotdeb.org/dotdeb.gpg | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install redis-server -y
 
-echo  -e '\e[0;32mInstalling nginx'
-sudo -s
-nginx=stable # use nginx=development for latest development version
-ubuntu=trusty # Replace this with your ubuntu version!
-echo "deb http://ppa.launchpad.net/nginx/$nginx/ubuntu $ubuntu main" > /etc/apt/sources.list.d/nginx-$nginx-$ubuntu.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C300EE8C
-apt-get update
-apt-get install -y nginx
+echo  -e '\e[0;32mCopying redis config'
+sudo cp /vagrant/redis.conf /etc/redis/
 
-
-echo  -e '\e[0;32mCopying nginx config'
-sudo cp /vagrant/nginx-config/default /etc/nginx/sites-enabled/default
-
-echo  -e '\e[0;32mReload nginx config'
-sudo nginx -s reload
+echo  -e '\e[0;32mRestarting redis service'
+sudo service redis-server restart
 

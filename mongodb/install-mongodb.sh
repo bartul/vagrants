d859ce9c98127be838a3f8b00e1a00ebe62c93af
Bartul Bonacic <bartul.bonacic@live.com>
2016-09-07 13:22:00 +0200

    Remove 127.0.0.1 binding from mongod.conf

--- mongodb/install-mongodb.sh
+++ mongodb/install-mongodb.sh
@@ -6,8 +6,9 @@ echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" 
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 
-# sudo service mongod start
+sudo sed --in-place '/bindIp: 127.0.0.1/d' /etc/mongod.conf
+
+sudo service mongod restart
 
 export LC_ALL=C
 
-# TODO: in /etc/mongod.conf comment line bindIp: 127.0.0.1

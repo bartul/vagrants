de5851dea35527a3af651a58735604a5dd5bc083
Bartul Bonacic <bartul.bonacic@live.com>
2016-11-02 09:28:28 +0100

    Fix Scylla install issue with karnel version less then 3.15

--- scylla/install-scylla.sh
+++ scylla/install-scylla.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
+export DEBIAN_FRONTEND=noninteractive
+
 echo  -e '\e[0;32mRefresh system'
 sudo apt-get update
-sudo apt-get upgrade -y
+sudo apt-get upgrade -y --force-yes -o Dpkg::Options::="--force-confnew"
 
 echo  -e '\e[0;32mInstalling essentials'
-apt-get install -y curl vim
+sudo apt-get install curl vim -y --force-yes -o Dpkg::Options::="--force-confnew"
 
 echo  -e '\e[0;32mInstaling scylla'
-sudo wget -O /etc/apt/sources.list.d/scylla.list http://downloads.scylladb.com/deb/ubuntu/scylla-1.2-trusty.list
+sudo wget -O /etc/apt/sources.list.d/scylla.list http://downloads.scylladb.com/deb/ubuntu/scylla-1.3-trusty.list
 
+# Go back to full scylla package once karnel is on version >=3.15
 sudo apt-get update
-sudo apt-get install scylla -y --force-yes
+sudo apt-get install scylla-server scylla-tools scylla-jmx -y --force-yes -o Dpkg::Options::="--force-confnew"
 
 echo  -e '\e[0;32mEnabling scylla developer mode'
 sudo /usr/lib/scylla/scylla_dev_mode_setup --developer-mode 1
